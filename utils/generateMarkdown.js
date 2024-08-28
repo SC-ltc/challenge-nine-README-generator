2a896070dfeb0654b580ff17f76201062a7ef8a6
Steven Chan <stevenchan@Stevens-MacBook-Air.local>
2024-08-28 15:41:46 -0700

    added function logic to generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,16 +11,81 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT License'){
+    return `[MIT License](https://choosealicense.com/licenses/mit/)`
+  }
+  if (license === 'Apache License 2.0'){
+    return `[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)`
+  }
+  if (license === 'GNU General Public License v3.0'){
+    return `[GNU General Public License v3.0](https://choosealicense.com/licenses/gpl-3.0/)`
+  }
+  if (license === 'None'){
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None'){
+    return ''
+  } else {
+    return `## License
+    ${renderLicenseBadge(license)}
+    This project is covered under the ${renderLicenseLink(license)}
+    `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   //template literal starts below
   return `# ${data.title}  
+
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  -[Description](#description)
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[Contributing](#contributing)
+  -[Tests](#tests)
+  -[Questions](#questions)
+  -[License](#license)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  If you have any questions, you may contact me at:
+
+  Github: [${data.github}](https://github.com/${data.github})
+  
+  Email: ${data.email}
+
+  
+  ${renderLicenseSection(data.license)}
 
 `;
 }
